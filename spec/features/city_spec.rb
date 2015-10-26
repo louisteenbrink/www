349541a2dc375853999bbb2cc1630beb1e26bcc2
Sebastien Saunier <seb@saunier.me>
2015-10-26 10:12:20 +0100

    Try to debug travis
    Former-commit-id: 170a27906d487d0640fa57deefc8416a8633ccd6

--- spec/features/city_spec.rb
+++ spec/features/city_spec.rb
@@ -4,12 +4,16 @@ RSpec.describe "City page", type: :feature do
   context "for Paris" do
     it "works in French" do
       visit '/fr/paris'
+      puts page.body
+
       expect(page).to have_selector("h1", text: "apprenez à coder")
       expect(page).to have_selector("strong", text: "Paris")
     end
 
     it "works in English" do
       visit '/en/paris'
+      puts page.body
+
       expect(page).to have_selector("h1", text: "learn to code")
       expect(page).to have_selector("strong", text: "Paris")
     end
