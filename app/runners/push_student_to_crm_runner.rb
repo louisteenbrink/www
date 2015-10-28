c1ce5d07a2f7ab110ae874a94389e8ebc90368a8
Sebastien Saunier <seb@saunier.me>
2015-10-28 12:37:12 +0100

    Push to CRM as well
    Former-commit-id: 941302fd97a1c38d99745f0616a5dbdf185dc479

---
+++ app/runners/push_student_to_crm_runner.rb
@@ -0,0 +1,34 @@
+class PushStudentToCrmRunner
+  def initialize(trello_card, apply)
+    @trello_card = trello_card
+    @apply = apply
+  end
+
+  def run(params)
+    url = "#{ENV['CRM_BASE_URL']}/api/v1/cards"
+
+    payload = {
+      card: {
+        trello_board_id: @trello_card.board.id,
+        trello_card_id: @trello_card.id,
+        email: @apply.email,
+        first_name: @apply.first_name,
+        last_name: @apply.last_name,
+        age: @apply.age,
+        phone_number: @apply.phone,
+        motivation: @apply.motivation,
+        price_cents: @apply.batch.price.cents,
+        price_currency: @apply.batch.price.currency.to_s
+      }
+    }
+
+    begin
+      RestClient.post url, payload.to_json,
+        content_type: :json,
+        accept: :json,
+        :'X-CRM-TOKEN' => ENV['CRM_TOKEN']
+    rescue Exception => e
+      puts e.message
+    end
+  end
+end
