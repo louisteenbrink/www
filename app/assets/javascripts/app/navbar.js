41fdf5cc298264799891baee0d60f61bbafe0891
Thomas Chretien <thomas@chretien.co>
2015-11-03 11:50:50 +0100

    add locales en.yml
    Former-commit-id: e9c7d71dd3e4c728b135576af3b59ac824238cdb

--- app/assets/javascripts/app/navbar.js
+++ app/assets/javascripts/app/navbar.js
@@ -6,21 +6,25 @@
 //   }
 // })
 
-$(document).ready(() => {
-  if ($('.header-fixed:visible').length) {
-    handleNavbar()
-    $(window).scroll(() => {
-      handleNavbar()
-    })
-  }
-})
+(function() {
+  var headerFixed = $('.header-fixed');
 
-function handleNavbar() {
-  if($(window).scrollTop() > $('.navbar-wagon').outerHeight()) {
-    $('.header-fixed').addClass('is-active');
-    $('.navbar-wagon-side .dropdown').removeClass('open')
-  } else {
-    $('.header-fixed').removeClass('is-active');
-    $('.full-item').removeClass('open');
+  $(document).ready(() => {
+    if (headerFixed.is(':visible')) {
+      handleNavbar()
+      $(window).scroll(() => {
+        handleNavbar()
+      })
+    }
+  })
+
+  function handleNavbar() {
+    if($(window).scrollTop() > $('.navbar-wagon').outerHeight()) {
+      headerFixed.addClass('is-active');
+      $('.navbar-wagon-side .dropdown').removeClass('open')
+    } else {
+      headerFixed.removeClass('is-active');
+      $('.full-item').removeClass('open');
+    }
   }
-}
+})();
