deecde3c178899f7b57c94be72bbf929dd30fddf
Sebastien Saunier <seb@saunier.me>
2015-11-13 13:59:47 +0100

    Redirect /:city to locale for the first time
    Former-commit-id: 6ee7507ce382ae74d613a8314d3fd52cf0e04109

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,12 +3,20 @@ class CitiesController < ApplicationController
 
   def show
     @city = @client.city(params[:city])
-    @teachers = @client.staff(params[:city])["teachers"]
-    @assistants = @client.staff(params[:city])["teacher_assistants"]
+    if I18n.locale == I18n.default_locale &&
+        @city['course_locale'].to_sym != I18n.locale &&
+        !session[:city_locale_already_forced] &&
+        (env["HTTP_ACCEPT_LANGUAGE"] || "").split(",").first =~ /^#{Regexp.quote(@city['course_locale'])}/
+      redirect_to city_path(params[:city], locale: @city['course_locale'])
+      session[:city_locale_already_forced] = true
+    else
+      @teachers = @client.staff(params[:city])["teachers"]
+      @assistants = @client.staff(params[:city])["teacher_assistants"]
 
-    meetup_cli = MeetupApiClient.new(@city["meetup_id"])
-    @meetup = { events: meetup_cli.meetup_events, infos: meetup_cli.meetup  }
-    session[:city] = @city['slug']
+      meetup_cli = MeetupApiClient.new(@city["meetup_id"])
+      @meetup = { events: meetup_cli.meetup_events, infos: meetup_cli.meetup  }
+      session[:city] = @city['slug']
+    end
   end
 
   private
