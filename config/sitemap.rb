2575550c31ad761f025dfe74d355a8efaeb1165b
Sebastien Saunier <seb@saunier.me>
2015-12-12 14:44:27 +0100

    Better fr title for city show pages and add priority to Sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,32 +5,32 @@ SitemapGenerator::Sitemap.create do
 
   STATIC_ROUTES.each do |_, locale_paths|
     locale_paths.each do |_, page|
-      add page
+      add page, priority: 0.8
     end
   end
   Static::CITIES.keys.each do |slug|
-    add slug
-    add "fr/#{slug}"
+    add slug, priority: 0.7
+    add "fr/#{slug}", priority: 0.7
   end
 
-  add "alumni"
-  add "fr/alumni"
+  add "alumni", priority: 0.8
+  add "fr/alumni", priority: 0.8
 
-  add "blog"
-  add "faq"
-  add "fr/faq"
-  add "stack"
-  add "fr/stack"
+  add "blog", priority: 0.8
+  add "faq", priority: 0.8
+  add "fr/faq", priority: 0.8
+  add "stack", priority: 0.6
+  add "fr/stack", priority: 0.6
 
   client = AlumniClient.new
   client.stories.each do |story|
     github_nickname = story["alumni"]["github_nickname"]
-    add "stories/#{github_nickname}"
-    add "fr/stories/#{github_nickname}"
+    add "stories/#{github_nickname}", priority: 0.6
+    add "fr/stories/#{github_nickname}", priority: 0.6
   end
 
   require "blog"
   Blog.new.all.each do |post|
-    add "blog/#{post.slug}"
+    add "blog/#{post.slug}", priority: 0.6
   end
 end
