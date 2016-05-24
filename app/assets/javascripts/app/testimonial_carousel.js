5367fca0a2e5641f20bc1e57a1ad3d2c26d0c907
Boris Paillard <boris.paillard@gmail.com>
2016-05-24 12:44:34 +0200

    add responsiveness for reviews carousel

--- app/assets/javascripts/app/testimonial_carousel.js
+++ app/assets/javascripts/app/testimonial_carousel.js
@@ -4,7 +4,36 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 2,
     autoplay: false,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 })
 
