3ff115188129db34d296fb5caa22000c2723855d
Sebastien Saunier <seb@saunier.me>
2016-05-26 16:34:24 +0200

    Remove schema info from controller

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: applies
-#
-#  id         :integer          not null, primary key
-#  first_name :string
-#  last_name  :string
-#  age        :integer
-#  email      :string
-#  phone      :string
-#  motivation :text
-#  batch_id   :integer
-#  city_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  tracked    :boolean          default(FALSE), not null
-#  source     :string
-#
-
 class AppliesController < ApplicationController
   include MoneyRails::ActionViewExtension
 
