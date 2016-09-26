5ae5b7e861146f52b432b51a3b6fab151200e9ad
Sebastien Saunier <seb@saunier.me>
2016-09-26 12:55:55 +0200

    HEC Apply

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220101603) do
+ActiveRecord::Schema.define(version: 20160926092227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,11 @@ ActiveRecord::Schema.define(version: 20160220101603) do
     t.text     "motivation"
     t.integer  "batch_id"
     t.integer  "city_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "tracked",    default: false, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "tracked",             default: false, null: false
     t.string   "source"
+    t.string   "codecademy_username"
   end
 
 end
