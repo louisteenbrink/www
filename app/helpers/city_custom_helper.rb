b67372ad7359983a6d8ff3c94a31ad915df42548
Sebastien Saunier <seb@saunier.me>
2016-11-16 21:17:53 +0000

    Custom course language for Montreal

--- app/helpers/city_custom_helper.rb
+++ app/helpers/city_custom_helper.rb
@@ -4,4 +4,10 @@ module CityCustomHelper
       montreal: t('montreal.post_price')
     }.stringify_keys
   end
+
+  def course_language
+    {
+      montreal: t('montreal.course_language')
+    }.stringify_keys
+  end
 end
