c5a9bbe3ca3b95590f5d07acbd035fcafe3235e0
Sebastien Saunier <seb@saunier.me>
2016-12-01 11:50:30 +0100

    Remove DemoDay from /alumni page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,28 +4,5 @@ class StudentsController < ApplicationController
     @projects = @client.projects("alumni_projects")
     @statistics = @client.statistics
     @stories = @client.stories
-    @demo_day = ({
-      fr: {
-        video_id: 'tf_oIjOxS2I',
-        date: '2 sept 2016',
-        city: 'Paris',
-        slug: 30,
-        start: 0
-      },
-      en: {
-        video_id: '83fw4h2vxCc',
-        date: 'Sept 10th, 2016',
-        city: 'London',
-        slug: 36,
-        start: 0
-      },
-      "pt-BR": {
-        video_id: 'zd9nbSmVO4I',
-        date: '2 de setembro 2016',
-        city: 'Saõ Paulo',
-        slug: 34,
-        start: 0
-      }
-    })[I18n.locale]
   end
 end
