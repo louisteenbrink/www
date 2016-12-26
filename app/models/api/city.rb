28f5a40ce4bfbdb5a3604041a924c69d961e3c0e
Sebastien Saunier <seb@saunier.me>
2016-12-26 17:21:13 +0100

    Upgrade encryptor

--- app/models/api/city.rb
+++ app/models/api/city.rb
@@ -19,9 +19,10 @@ module Api
     def mailchimp_api_key
       @mailchimp_api_key ||= (
         return nil if @json['encrypted_mailchimp_api_key'].blank?
-        key   = ActiveSupport::KeyGenerator.new(ENV['ALUMNI_WWW_ENCRYPING_KEY']).generate_key(@json['id'].to_s)
-        crypt = ActiveSupport::MessageEncryptor.new(key)
-        crypt.decrypt_and_verify(@json["encrypted_mailchimp_api_key"])
+        parts = @json['encrypted_mailchimp_api_key'].split("@")
+        key   = ActiveSupport::KeyGenerator.new(ENV['ALUMNI_WWW_ENCRYPING_KEY']).generate_key(parts.first)
+        crypt = ActiveSupport::MessageEncryptor.new(key[0..31])
+        crypt.decrypt_and_verify(parts.last)
       )
     end
   end
