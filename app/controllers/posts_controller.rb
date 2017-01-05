05c9f335db834cc9d08cbe240b4c8ca54b480255
Sebastien Saunier <seb@saunier.me>
2017-01-05 09:46:29 +0100

    Hide drift on blog pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 require "blog"
 
 class PostsController < ApplicationController
+  before_action :hide_drift
+
   def index
     @posts = Blog.new.all
   end
@@ -12,5 +14,11 @@ class PostsController < ApplicationController
   def show
     @post = Blog.new.post(params[:slug])
     render_404 if @post.nil?
+  end
+
+  private
+
+  def hide_drift
+    @hide_drift = true
   end
 end
