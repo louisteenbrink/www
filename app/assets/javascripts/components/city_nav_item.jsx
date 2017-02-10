c993662affa2997e2df438bcb8545e937ffcc709
Sebastien Saunier <seb@saunier.me>
2017-02-10 14:20:59 +0100

    More invariant fixes

--- app/assets/javascripts/components/city_nav_item.jsx
+++ app/assets/javascripts/components/city_nav_item.jsx
@@ -21,10 +21,12 @@ class CityNavItem extends React.Component {
             {city.name}
           </div>
           <div className='city-nav-item-first-batch'>
-           {this.props.firstBatch.starts_at_short}
-           <span> / </span>
-           {this.props.i18n.pre_course_language}
-           {this.props.i18n.course_language[city.slug] || this.props.i18n['language_' + city.course_locale]}
+            {
+              this.props.firstBatch.starts_at_short
+              + " / "
+              + this.props.i18n.pre_course_language
+              + (this.props.i18n.course_language[city.slug] || this.props.i18n['language_' + city.course_locale])
+            }
           </div>
         </div>
         <div className='banner-city-miniature' style={miniatureStyle} />
