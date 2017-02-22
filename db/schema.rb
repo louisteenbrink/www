fdef1fb9a5151b3d575c4abe3e5ae155a4a60e6a
cedricmenteau <me.cedric@gmail.com>
2017-02-22 16:09:10 +0100

    Add cocoon link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216114848) do
+ActiveRecord::Schema.define(version: 20170123122541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 20161216114848) do
     t.string   "link"
     t.string   "facebook_url"
     t.index ["user_id"], name: "index_lives_on_user_id", using: :btree
+  end
+
+  create_table "testimonials", force: :cascade do |t|
+    t.string   "source"
+    t.string   "source_id"
+    t.string   "github_nickname"
+    t.text     "content"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
