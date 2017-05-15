32c9af00233c74d8e8667512e9441d8db2444c3b
Sebastien Saunier <seb@saunier.me>
2017-05-15 18:11:02 +0200

    Count prospects

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -17,11 +17,12 @@ class Prospect < ApplicationRecord
   private
 
   def notify_slack
+    count_prospect_for_today = Prospect.where("created_at >= ?", Date.today).count
     NotifySlack.perform_later({
       "channel": Rails.env.development? ? "test" : "incoming",
       "username": "www",
       "icon_url": "https://raw.githubusercontent.com/lewagon/mooc-images/master/slack_bot.png",
-      "text": "New prospect for the free Web Development Basics track on *#{from_path}*: #{email}"
+      "text": "Today's #{count_prospect_for_today}#{count_prospect_for_today.ordinal} prospect for the free Web Development Basics track on *#{from_path}*: #{email}"
     })
   end
 end
