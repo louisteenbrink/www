efabd603bf3b38d6331daf5ccaba9994068549a3
cedricmenteau <me.cedric@gmail.com>
2017-06-05 12:44:28 +0200

    Cleaning Shema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,87 +10,84 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605083431) do
+ActiveRecord::Schema.define(version: 20170514130732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "applies", id: :serial, force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "age"
-    t.string "email"
-    t.string "phone"
-    t.text "motivation"
-    t.integer "batch_id"
-    t.integer "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "tracked", default: false, null: false
-    t.string "source"
-    t.string "codecademy_username"
-    t.string "linkedin"
+  create_table "applies", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "motivation"
+    t.integer  "batch_id"
+    t.integer  "city_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "tracked",             default: false, null: false
+    t.string   "source"
+    t.string   "codecademy_username"
+    t.string   "linkedin"
   end
 
-  create_table "attachinary_files", id: :serial, force: :cascade do |t|
-    t.string "attachinariable_type"
-    t.integer "attachinariable_id"
-    t.string "scope"
-    t.string "public_id"
-    t.string "version"
-    t.integer "width"
-    t.integer "height"
-    t.string "format"
-    t.string "resource_type"
+  create_table "attachinary_files", force: :cascade do |t|
+    t.string   "attachinariable_type"
+    t.integer  "attachinariable_id"
+    t.string   "scope"
+    t.string   "public_id"
+    t.string   "version"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "format"
+    t.string   "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
   end
 
-  create_table "lives", id: :serial, force: :cascade do |t|
-    t.string "category"
-    t.integer "user_id"
+  create_table "lives", force: :cascade do |t|
+    t.string   "category"
+    t.integer  "user_id"
     t.datetime "started_at"
     t.datetime "ended_at"
-    t.string "batch_slug"
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "city_slug"
-    t.string "subtitle"
-    t.string "link"
-    t.string "facebook_url"
-    t.index ["user_id"], name: "index_lives_on_user_id"
+    t.string   "batch_slug"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "city_slug"
+    t.string   "subtitle"
+    t.string   "link"
+    t.string   "facebook_url"
+    t.index ["user_id"], name: "index_lives_on_user_id", using: :btree
   end
 
-  create_table "prospects", id: :serial, force: :cascade do |t|
-    t.string "email"
+  create_table "prospects", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "from_path"
+    t.string   "from_path"
     t.string "city"
   end
 
-  create_table "testimonials", id: :serial, force: :cascade do |t|
-    t.string "source"
-    t.string "source_id"
-    t.string "github_nickname"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.integer "sign_in_count", default: 0, null: false
+  create_table "users", force: :cascade do |t|
+    t.integer  "sign_in_count",      default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "uid"
-    t.string "github_nickname"
-    t.string "gravatar_url"
-    t.datetime "created_at", null: false
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "uid"
+    t.string   "github_nickname"
+    t.string   "gravatar_url"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+  end
+
+  add_foreign_key "lives", "users"
+end
+t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
