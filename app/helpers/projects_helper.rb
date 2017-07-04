c57f9a4d23d543148bb7eb9fc808faa304f6de91
kevin robert <kevin.j.robert@gmail.com>
2017-07-05 01:05:07 +0200

    thumbnail_url

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,7 +3,7 @@ module ProjectsHelper
     cloudinary_url(project["cloudinary_image_id"], width: 700, height: 365, format: 'jpg', quality: 40)
   end
 
-  def thumbnail(project)
+  def thumbnail_url(project)
     cloudinary_url(project["cloudinary_image_id"], width: 320, height: 167, format: 'jpg', quality: 20)
   end
 end
