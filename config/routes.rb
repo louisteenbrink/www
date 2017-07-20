3f8d99f8529b0217b4fd7be93c7781ec8926700f
Sébastien Saunier <seb@saunier.me>
2017-07-20 16:21:59 +0200

    /learn/:city

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   get 'sp', to: redirect('pt-BR/sao-paulo')
   get 'bh', to: redirect('pt-BR/belo-horizonte')
   get 'en', to: redirect('/')
-  get 'learn', to: redirect('learn-to-code'), as: :learn_to_code
+  get 'learn(/:city)', to: redirect { |params, _| params[:city].blank? ? 'learn-to-code' : "learn-to-code/#{params[:city]}" }, as: :learn_to_code
   get 'stories', to: redirect('alumni')
   get 'fr/stories', to: redirect('fr/alumni')
   get 'en/*path', to: redirect { |path_params, req| path_params[:path] }
@@ -75,7 +75,7 @@ Rails.application.routes.draw do
     get "jobs", to: "pages#show", template: "jobs", as: :jobs
     get "stack", to: "pages#stack", template: "stack", as: :stack
     get "employers", to: "pages#employers", template: "employers", as: :employers
-    get "learn-to-code", to: "pages#learn", template: "learn", as: :learn
+    get "learn-to-code(/:city)", to: "pages#learn", template: "learn", as: :learn
     get "enterprise", to: "pages#enterprise", template: "enterprise", as: :enterprise
     get "blog/videos", to: "posts#videos", template: "videos", as: :videos
     get "blog/all", to: "posts#all", template: "all", as: :all
