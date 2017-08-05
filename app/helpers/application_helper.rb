9880eb013e07ecabae73177f200c7e4acae5bc88
Sébastien Saunier <seb@saunier.me>
2017-08-05 18:25:41 +0200

    proxy_path => proxy_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,20 +38,20 @@ module ApplicationHelper
   end
 
   def author_image_tag(slug)
-    image_tag proxy_path(url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/authors/#{slug}")
+    image_tag proxy_url(url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/authors/#{slug}")
   end
 
   def post_image_url(slug)
     return slug if slug =~ /^http/  # Handy to quickly try a post background image from a URL.
-    proxy_path url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/posts/#{slug}"
+    proxy_url url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/posts/#{slug}"
   end
 
   def content_post_image_tag(slug)
-    image_tag proxy_path(url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/posts/#{slug}")
+    image_tag proxy_url(url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/posts/#{slug}")
   end
 
   def video_image_tag(slug)
-    proxy_path url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/videos/#{slug}"
+    proxy_url url: "https://raw.githubusercontent.com/lewagon/www-images/master/blog/videos/#{slug}"
   end
 
   def cl_adaptive_image_tag(image_path, opt={})
--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -37,7 +37,8 @@ class Testimonial
   end
 
   def picture_url
-    proxy_path url: "https://raw.githubusercontent.com/lewagon/www-images/master/testimonials/#{@hash['picture']}"
+    proxy_url host: (ENV['HOST'] || "localhost:3000"),
+      url: "https://raw.githubusercontent.com/lewagon/www-images/master/testimonials/#{@hash['picture']}"
   end
 
   def cache_key
