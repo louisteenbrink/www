b6cb9962c48ade5c67826bd75f460265bec4a100
Sébastien Saunier <seb@saunier.me>
2017-08-05 18:14:10 +0200

    Fix caching for GitHub proxy path

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -8,13 +8,10 @@ class ProxyController < ActionController::Base
   def show
     url = params[:url]
     if ALLOWED_ORIGINS.any? { |origin| url =~ origin }
-      data = from_cache(:proxy, url, expire: 1.year) do
+      data = from_cache(:proxy, url, expire: 1.month) do
         Net::HTTP.get_response(URI.parse(url))
       end
-      # response.cache_control[:public] = true
-      # response.headers['Cache-Control'] = 'public, max-age=31536000'
-      # response.headers['Expires'] = 1.year.from_now.to_formatted_s(:rfc822)
-      expires_in 1.year, public: true, "must-revalidate" => false
+      expires_in 1.month, public: true
       send_data data.body, type: data.content_type, disposition: :inline
     else
       render plain: 'Not Authorized', status: 403
