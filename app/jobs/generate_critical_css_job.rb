5a82afe2afc05e4899b864f78dc831dd40304fe9
Sébastien Saunier <seb@saunier.me>
2017-08-06 17:46:57 +0200

    Remove semaphor in favor of unique sidekiq gem extension

--- app/jobs/generate_critical_css_job.rb
+++ app/jobs/generate_critical_css_job.rb
@@ -4,15 +4,9 @@ class GenerateCriticalCssJob
                   unique: :until_executing,
                   unique_args: :unique_args
 
-  CSS_SEMAPHOR_NAMESPACE = 'critical-path-css-semaphor'.freeze
-
   def perform(route)
-    return if route.nil?
-    return if Rails.cache.exist?(route, namespace: CSS_SEMAPHOR_NAMESPACE)
-    return unless CriticalPathCss.fetch(route).empty?
-    Rails.cache.write(route, 'generating', { namespace: CSS_SEMAPHOR_NAMESPACE, expires_in: 10.minutes })
+    return if route.nil? || CriticalPathCss.fetch(route).empty?
     CriticalPathCss.generate route
-    Rails.cache.delete(route, namespace: CSS_SEMAPHOR_NAMESPACE)
   end
 
   def self.unique_args(args)
