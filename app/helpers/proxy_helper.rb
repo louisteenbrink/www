e63118c6ff9ef4d24ac3e2c6b57a0889c77ee8d2
Sébastien Saunier <seb@saunier.me>
2017-08-06 15:57:49 +0200

    Add inline documentation for ProxyHelper#proxy_url_with_signature

--- app/helpers/proxy_helper.rb
+++ app/helpers/proxy_helper.rb
@@ -1,4 +1,10 @@
 module ProxyHelper
+  # Usage:
+  # proxy_url_with_signature(url: url, height: height, width: width, quality: quality)
+  #   - url: an external URL you want to proxy
+  #   - width (optional): if you want to perform an image resize, use this
+  #   - height (optional): Same as width
+  #   - quality (optional): Image will be converted to JPG by the proxy. Specify quality if necessary (default: 90)
   def proxy_url_with_signature(args = {})
     signature = ProxyService.new.sign(args)
     proxy_image_url args.merge(signature: signature)
