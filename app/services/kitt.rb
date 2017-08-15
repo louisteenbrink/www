bb2f7337bcdf5d6ebce7951333915d99adbd1d9b
Sébastien Saunier <seb@saunier.me>
2017-08-16 00:20:11 +0200

    GraphQL client caching. ping @krokrob

--- app/services/kitt.rb
+++ app/services/kitt.rb
@@ -2,10 +2,46 @@ require "graphql/client"
 require "graphql/client/http"
 
 module Kitt
-  base_url = ENV.fetch('KITT_BASE_URL', 'https://kitt.lewagon.com/graphql')
+  base_url = ENV.fetch('KITT_BASE_URL', 'https://kitt.lewagon.com') + "/graphql"
   HTTP = GraphQL::Client::HTTP.new(base_url)
   Schema = GraphQL::Client.load_schema(HTTP)
-  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+  class Client
+    include Singleton
+    include Cache
+    DEFAULT_EXPIRE = 1.day
+
+    class Error < StandardError
+      def initialize(errors)
+        @errors = errors
+      end
+
+      def to_s
+        "#{@errors.messages.to_hash}, #{@errors.details.to_hash}"
+      end
+    end
+
+    class << self
+      delegate :query, to: :instance
+      delegate :parse, to: :instance
+    end
+
+    def initialize
+      @client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+    end
+
+    delegate :parse, to: :@client
+
+    def query(definition, variables: {}, context: {})
+      result = from_cache(definition.document.to_query_string, variables, expire: DEFAULT_EXPIRE) do
+        Rails.logger.debug "GRAPHQL QUERY: #{definition.document.to_query_string} (VARIABLES: #{variables})"
+        response = @client.query(definition, variables: variables, context: context)
+        raise Error.new(response.errors) if response.errors.messages.any?
+        response.original_hash
+      end
+      data, extensions = result.values_at("data", "extensions")
+      GraphQL::Client::Response.new(result, data: definition.new(data), extensions: extensions)
+    end
+  end
 end
 
   # def products(slugs = [])
