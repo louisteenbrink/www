32188fb715a533a7e64f21f20459896ea42f6721
cedricmenteau <me.cedric@gmail.com>
2017-10-01 18:33:03 +0200

    Send content email if next event runs in more than 7 days

--- app/jobs/contact_prospects_job.rb
+++ app/jobs/contact_prospects_job.rb
@@ -1,13 +1,21 @@
 class ContactProspectsJob < ActiveJob::Base
   TIME_SPAN = 7
+  TIME_SPAN_NEXT_EVENT = 8
   def perform
     Prospect.where('created_at >= ? AND created_at < ?', Time.now - TIME_SPAN.days , Time.now).each do |prospect|
       city = AlumniClient.new.city(prospect.city)
-      meetup = MeetupApiClient.new(city.meetup_id)
-      events = meetup.meetup_events
+      meetup_cli = MeetupApiClient.new(city.meetup_id)
+      meetups = meetup_cli.meetup_events
 
-      if events.count >= 1
-        ProspectMailer.send_event(prospect).deliver_now
+      if meetups.count >= 1
+        meetup = { event: meetup_cli.meetup_events.first }
+        meetup_time = Time.at(meetup[:event]["time"] / 1000)
+
+        if meetup_time < Time.now + TIME_SPAN_NEXT_EVENT.days
+          ProspectMailer.send_event(prospect).deliver_now
+        else
+          ProspectMailer.send_content(prospect).deliver_now
+        end
       else
         ProspectMailer.send_content(prospect).deliver_now
       end
--- app/mailers/prospect_mailer.rb
+++ app/mailers/prospect_mailer.rb
@@ -1,4 +1,5 @@
 class ProspectMailer < ApplicationMailer
+
   def invite(prospect)
     @prospect = prospect
 
