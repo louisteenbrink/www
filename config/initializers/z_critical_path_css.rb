4cd4cffe0c4bde96d4dce930c064aeb6e3ec708e
Sébastien Saunier <seb@saunier.me>
2017-11-23 15:31:48 +0100

    Hotfix on critical CSS path

--- config/initializers/z_critical_path_css.rb
+++ config/initializers/z_critical_path_css.rb
@@ -1,1 +1,1 @@
-CRITICAL_PATH_CSS_ROUTES = CriticalPathCss::CssFetcher.new.instance_variable_get(:"@config").instance_variable_get(:"@configurations")["routes"]
+CRITICAL_PATH_CSS_ROUTES = CriticalPathCss::CssFetcher.new.instance_variable_get(:"@config").instance_variable_get(:"@configurations")["routes"] || []
