afee3813752c614d466a50018bb18b8345248330
kevin robert <kevin.j.robert@gmail.com>
2017-11-23 18:02:36 +0100

    fix /projetcs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,9 @@ class ProjectsController < ApplicationController
     @top_bar_url = demoday_index_path
 
     if request.format.html? || params[:project_page]
-      @projects = @kitt_client.products(Static::PROJECTS[:top])
+      @projects = Static::PROJECTS[:top].map do |project_slug|
+        Kitt::Client.query(Product::Query, variables: { slug: project_slug }).data.product
+      end
       @projects = Kaminari.paginate_array(@projects).page(params[:project_page]).per(10)
     end
   end
