f5ae34a3603051475e01109034933c4b0c45a9ea
Sébastien Saunier <seb@saunier.me>
2017-11-23 17:35:29 +0100

    Use price cents/currency from GraphQL Kitt API

--- app/runners/push_to_trello_runner.rb
+++ app/runners/push_to_trello_runner.rb
@@ -63,7 +63,7 @@ EOF
 
 ## Invoice
 
-Price: #{price.currency.iso_code} #{humanized_money price}
+Price: #{price.currency} #{humanized_money price}
 
 ## Referrer
 
@@ -132,6 +132,6 @@ EOF
   end
 
   def price
-    Rails.env.production? ? @apply.batch.price : Money.new(1000, 'EUR')
+    Rails.env.production? ? Money.new(@apply.batch.price.cents, @apply.batch.price.currency) : Money.new(1000, 'EUR')
   end
 end
