368b1443c5d3bfb18293ef7f2e9a3c3f7593538c
Sébastien Saunier <seb@saunier.me>
2017-11-23 23:24:21 +0100

    Fix custom language for brazilian cities

--- app/helpers/city_custom_helper.rb
+++ app/helpers/city_custom_helper.rb
@@ -12,6 +12,9 @@ module CityCustomHelper
       shanghai: t('shanghai.course_language'),
       chengdu: t('chengdu.course_language'),
       tokyo: t('tokyo.course_language'),
+      recife: t('recife.course_language'),
+      :"belo-horizonte" => t('belo-horizonte.course_language'),
+      :"sao-paulo" => t('tokyo.course_language'),
       london: "English (Full-Time)", # Needed for PCDL / interest free lonas from the government.
     }.stringify_keys
   end
