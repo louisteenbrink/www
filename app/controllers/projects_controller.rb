f212acff2c81a59566e8ad4a1e073310cb68cd29
Sébastien Saunier <seb@saunier.me>
2017-12-13 10:44:03 +0100

    Fix recurring error on /projects for bots

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,11 +4,9 @@ class ProjectsController < ApplicationController
     @top_bar_cta = I18n.t('.top_bar_cta')
     @top_bar_url = demoday_index_path
 
-    if request.format.html? || params[:project_page]
-      @projects = Static::PROJECTS[:top].map do |project_slug|
-        Kitt::Client.query(Product::Query, variables: { slug: project_slug }).data.product
-      end
-      @projects = Kaminari.paginate_array(@projects).page(params[:project_page]).per(10)
+    @projects = Static::PROJECTS[:top].map do |project_slug|
+      Kitt::Client.query(Product::Query, variables: { slug: project_slug }).data.product
     end
+    @projects = Kaminari.paginate_array(@projects).page(params[:project_page]).per(10)
   end
 end
