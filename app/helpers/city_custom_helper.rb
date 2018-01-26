1027d3bb742cb1d83688c529d77cd68dc0c0467b
Sébastien Saunier <seb@saunier.me>
2018-01-26 21:54:32 +0100

    Closing Recife

--- app/helpers/city_custom_helper.rb
+++ app/helpers/city_custom_helper.rb
@@ -12,7 +12,6 @@ module CityCustomHelper
       shanghai: t('shanghai.course_language'),
       chengdu: t('chengdu.course_language'),
       tokyo: t('tokyo.course_language'),
-      recife: t('recife.course_language'),
       :"belo-horizonte" => t('belo-horizonte.course_language'),
       :"sao-paulo" => t('sao-paulo.course_language'),
       london: "English (Full-Time)", # Needed for PCDL / interest free lonas from the government.
