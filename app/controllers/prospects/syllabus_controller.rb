ce20706ac661c8afc4b0d943dc080df5951bd240
Dimitri Bosch <dimitribosch@hotmail.com>
2018-03-15 17:07:01 +0100

    Add tests for Download syllabus form in modal

--- app/controllers/prospects/syllabus_controller.rb
+++ app/controllers/prospects/syllabus_controller.rb
@@ -2,6 +2,7 @@ class Prospects::SyllabusController < ApplicationController
   def create
     @prospect = Prospect.find_or_create_by(
       email: params[:prospect][:email],
+      name: params[:prospect][:name],
       from_path: params[:prospect][:from_path],
       origin: params[:prospect][:origin] || "syllabus",
       city: params[:prospect][:city])
